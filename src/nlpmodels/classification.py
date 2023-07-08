c8fdc9fbad63e5e643504dc5963bc7551eeb6a69
Jose Elier Fajardo <joselierfajardo@gmail.com>
2023-07-08 16:12:18 -0400

    Added train and classify methods

--- src/nlpmodels/classification.py
+++ src/nlpmodels/classification.py
@@ -1,6 +1,5 @@
 import math
-from spacy.tokenizer import Tokenizer
-from spacy.lang.en import English
+import numpy as np
 
 class NaiveBayesClassifier:
 	def __init__(self):
@@ -9,18 +8,64 @@ class NaiveBayesClassifier:
 		self.categoryProbs = {} # P(Ck) = C(Ck) / docsLen
 		self.wordUnionCategoryProbs = {} # P(Wi,Ck) = C(Wi,Ck) / corpusLen
 
+		self.corpusLen = 0
+		self.docsLen = 0
+		self.uniqueCategories = set()
+		self.uniqueWords = set()
+
 	def _propertyCount(self):
 		categoryCount = {}
 		wordUnionCategoryCount = {}
 
 		for i in range(len(self.corpus)):
+			self.docsLen += 1
+			self.uniqueCategories.add(self.categories[i])
 			try:
 				categoryCount[self.categories[i]] += 1
 			except KeyError:
 				categoryCount[self.categories[i]] = 1
 				
 			for word in self.corpus[i]:
+				self.corpusLen += 1
+				self.uniqueWords.add(word)
 				try:
 					wordUnionCategoryCount[f'{word},{self.categories[i]}'] += 1
 				except KeyError:
 					wordUnionCategoryCount[f'{word},{self.categories[i]}'] = 1
+
+		return categoryCount, wordUnionCategoryCount
+	
+	def train(self, corpus, categories):
+		self.corpus = corpus
+		self.categories = categories
+
+		categoryCount, wordUnionCategoryCount = self._propertyCount()
+
+		# Category Probability Calculation
+		for cat in self.uniqueCategories:
+			self.categoryProbs[cat] = categoryCount[cat] / self.docsLen
+
+			# Word Union Category Probability
+			for word in self.uniqueWords:
+				try:
+					key = f'{word},{cat}'
+					self.wordUnionCategoryProbs[key] = wordUnionCategoryCount[key] / self.corpusLen
+				except KeyError:
+					# Laplace smoothing to prevent assigning 0 probability to unseen events 
+					# as probability 0 causes problems when using logarithms
+					self.wordUnionCategoryProbs[key] = 1 / self.corpusLen
+
+	def classify(self, doc):
+		categories = list(self.uniqueCategories)
+		categoryProbPaths = []
+		for category in categories:
+			categoryProb = math.log10(self.categoryProbs[category])
+			for word in doc:
+				try:
+					categoryProb += math.log10(self.wordUnionCategoryProbs[f'{word},{category}'] / self.categoryProbs[category])
+				except KeyError:
+					categoryProb += 0 	
+			categoryProbPaths.append(categoryProb)
+
+		predictedCategory = categories[np.argmax(categoryProbPaths)]
+		return predictedCategory
